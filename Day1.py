dcdde2afb8d4a2b6d5bb492c89b8a17ffd5c4d12
Wangechi-waweru <wawerulynn@gmail.com>
2024-08-09 20:14:16 +0300

    Adding existing file

---
+++ Day1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Generator.")
+city = input("What is the name of the city you grew up in?\n")
+petName = input("What's your pet's name?\n")
+bandName = city + " " + petName
+print(f"Your band name could be {bandName}.")
